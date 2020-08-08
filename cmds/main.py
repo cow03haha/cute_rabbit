5b4716ae413841a577a0a4b04e86c4b09db3775e
cow03haha <44705326+cow03haha@users.noreply.github.com>
2020-08-08 17:42:38 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from cores.classes import Cog_Extension
 from bot import bcdata
+import datetime
+import pytz
 
 class Main(Cog_Extension):
     #ping指令(discord給的單位為秒，直接乘於1000得到毫秒(ms)，在用round取整數)
@@ -14,11 +16,15 @@ class Main(Cog_Extension):
     async def say(self, ctx, message):
         await ctx.send(message)
     
-    #發送本地圖片(如果想發送網路圖片，直接send網址就好)
     @commands.command()
     async def about(self, ctx):
-        pic = discord.File(bcdata['info_pic'])#發送檔案的處理方式
-        await ctx.send(file = pic)#用file來定要發送檔案
+        tw = pytz.timezone('Asia/Taipei')
+        embed=discord.Embed(title="about", description="開心莊園專用bot", color=0xf5ed00, timestamp=datetime.datetime.now(tz=tw))
+        embed.set_author(name="cow03", icon_url="https://i.imgur.com/QinbCaq.png")
+        embed.set_thumbnail(url="https://i.imgur.com/za5ATTg.png")
+        embed.add_field(name="作者", value="cow03#7829", inline=True)
+        embed.add_field(name="support server", value="[link](https://discord.gg/DRqZk6Y)", inline=True)
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Main(bot))
