426e43f61ffb16d2170ef26e4111696b8163577e
cow03haha <44705326+cow03haha@users.noreply.github.com>
2020-08-08 14:19:12 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -4,3 +4,11 @@ from discord.ext import commands
 class Main(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    #ping指令(discord給的單位為秒，直接乘於1000得到毫秒(ms)，在用round取整數)
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f'延遲：{round(self.bot.latency*1000)} 毫秒(ms)')
+
+def setup(bot):
+    bot.add_cog(Main(bot))
