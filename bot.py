8270c802a0581758a0b65e5f44bf7ba748b67fd5
cow03haha <44705326+cow03haha@users.noreply.github.com>
2020-08-08 10:46:05 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,4 +7,15 @@ bot = commands.Bot(command_prefix='/')
 async def on_ready():
     print("bot online!")
 
+@bot.event
+async def on_member_join(member):
+#    print(f'{member} 加入了牛牛神殿')
+    channel = bot.get_channel(620192290547433492)
+    await channel.send(f'{member} 加入了牛牛神殿')
+@bot.event
+async def on_member_remove(member):
+#    print(f'{member} 離開了牛牛神殿')
+    channel = bot.get_channel(620192290547433492)
+    await channel.send(f'{member} 離開了牛牛神殿')
+
 bot.run('NjIwMTc2NTg1Mzk1NDA0ODMw.XXS-dA.dnIgi6ZeO7zwWo5Dcc6c4CBDU6s')
