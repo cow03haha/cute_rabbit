a4fb1e234ab852f222cea3201148889d4ed8f3f6
cow03haha <44705326+cow03haha@users.noreply.github.com>
2020-08-08 15:36:18 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -1,10 +1,8 @@
 import discord
 from discord.ext import commands
+from cores.classes import Cog_Extension
 
-class Main(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
+class Main(Cog_Extension):
     #ping指令(discord給的單位為秒，直接乘於1000得到毫秒(ms)，在用round取整數)
     @commands.command()
     async def ping(self, ctx):
