e3a187ba18a0128e7b6671a71688ec5cea3a81e2
cow03haha <44705326+cow03haha@users.noreply.github.com>
2020-08-08 11:00:10 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,10 +12,16 @@ async def on_member_join(member):
 #    print(f'{member} 加入了牛牛神殿')
     channel = bot.get_channel(620192290547433492)
     await channel.send(f'{member} 加入了牛牛神殿')
+
 @bot.event
 async def on_member_remove(member):
 #    print(f'{member} 離開了牛牛神殿')
     channel = bot.get_channel(620192290547433492)
     await channel.send(f'{member} 離開了牛牛神殿')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'延遲：{round(bot.latency*1000)} 毫秒(ms)')
+
 
 bot.run('NjIwMTc2NTg1Mzk1NDA0ODMw.XXS-dA.dnIgi6ZeO7zwWo5Dcc6c4CBDU6s')
