9dabd9964c76e61279666496e147787327cc846c
cow03haha <44705326+cow03haha@users.noreply.github.com>
2020-08-08 15:38:16 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,18 @@ bot = commands.Bot(command_prefix='/')
 async def on_ready():
     print("bot online!")
 
+@bot.event
+async def on_member_join(self, member):
+    #print(f'{member} 加入了牛牛神殿')
+    channel = bot.get_channel(int(bcdata['join_channel']))
+    await channel.send(f'{member} 加入了牛牛神殿')
+
+@bot.event
+async def on_member_remove(self, member):
+    #print(f'{member} 離開了牛牛神殿')
+    channel = bot.get_channel(int(bcdata['leave_channel']))
+    await channel.send(f'{member} 離開了牛牛神殿')
+
 for filename in os.listdir('./cmds'):
     if filename.endswith('.py'):
         bot.load_extension(f'cmds.{filename[:-3]}')
