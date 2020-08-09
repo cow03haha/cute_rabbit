7edfe72009ef00d9dab66772640e59625869fe32
cow03haha <44705326+cow03haha@users.noreply.github.com>
2020-08-09 19:11:38 +0800

    Update admin.py

--- cmds/admin.py
+++ cmds/admin.py
@@ -17,5 +17,15 @@ class Admin(Cog_Extension):
         channel = self.bot.get_channel(int(bcdata['leave_channel']))
         await channel.send(f'{member} 離開了牛牛神殿')
 
+    @commands.command()
+    async def clear(self, ctx, count:int):
+        if ctx.author.id == 315414910689476609:
+            await ctx.channel.purge(limit=count+1)
+            await ctx.send(f'清理{count}條訊息成功')
+            await ctx.channel.purge(limit=1)
+        else:
+            await ctx.send('只有牛牛能用這個指令')
+            await ctx.channel.purge(limit=2)
+
 def setup(bot):
     bot.add_cog(Admin(bot))
