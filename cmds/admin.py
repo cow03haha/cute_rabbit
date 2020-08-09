252d3853740c1b9b2d84f1b3085b1b23cf5b3c6b
cow03haha <44705326+cow03haha@users.noreply.github.com>
2020-08-09 22:53:01 +0800

    Update admin.py

--- cmds/admin.py
+++ cmds/admin.py
@@ -19,6 +19,7 @@ class Admin(Cog_Extension):
 
     @commands.command()
     async def clear(self, ctx, count:int):
+        '''清理當前頻道的訊息'''
         if ctx.author.id == 315414910689476609:
             await ctx.channel.purge(limit=count+1)
             await ctx.send(f'清理{count}條訊息成功')
@@ -26,6 +27,14 @@ class Admin(Cog_Extension):
         else:
             await ctx.send('只有牛牛能用這個指令')
             await ctx.channel.purge(limit=2)
+    
+    @commands.command()
+    async def poweroff(self, ctx):
+        '''關閉bot'''
+        if ctx.author.id == 315414910689476609:
+            exit()
+        else:
+            await ctx.send('只有牛牛能用這個指令')
 
 def setup(bot):
     bot.add_cog(Admin(bot))
