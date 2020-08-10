4b657960bfdd719a6ce99360c82b0d67cb3b2345
cow03haha <44705326+cow03haha@users.noreply.github.com>
2020-08-10 21:07:04 +0800

    Update admin.py

--- cmds/admin.py
+++ cmds/admin.py
@@ -22,6 +22,25 @@ class Admin(Cog_Extension):
         '''清理當前頻道的訊息'''
         await ctx.channel.purge(limit=count+1)
         await ctx.send(f'清理{count}條訊息成功', delete_after=3)
+    
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        if payload.message_id == 742363869015703593:
+            if str(payload.emoji) == '<:blobpopcorn:713660254440783932>':
+                guild = self.bot.get_guild(payload.guild_id)#取得server id
+                role = guild.get_role(742364064369475644)#取得role資料
+                await payload.member.add_roles(role)#給予role
+                await payload.member.send(f'領取 **{role}** 身分組成功')
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        if payload.message_id == 742363869015703593:
+            if str(payload.emoji) == '<:blobpopcorn:713660254440783932>':
+                guild = self.bot.get_guild(payload.guild_id)#取得server id
+                role = guild.get_role(742364064369475644)#取得role資料
+                user = guild.get_member(payload.user_id)
+                await user.remove_roles(role)#移除role
+                await user.send(f'移除 **{role}** 身分組成功')
 
 def setup(bot):
     bot.add_cog(Admin(bot))
