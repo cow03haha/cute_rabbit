a5f6bee75d2f46568ccc7012ddb19af08652ff9b
cow03haha <44705326+cow03haha@users.noreply.github.com>
2020-08-10 16:46:09 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,39 +29,29 @@ def check_owner(ctx):
 @bot.command()
 async def load(ctx, extension):
     '''載入特定模組'''
-    if ctx.author.id == 315414910689476609:
-        bot.load_extension(f'cmds.{extension}')
-        await ctx.send(f'{extension} 模組載入完成')
-    else:
-        await ctx.send('只有牛牛能用這個指令')
+    bot.load_extension(f'cmds.{extension}')
+    await ctx.send(f'{extension} 模組載入完成')
+
 @commands.check(check_owner)
 @bot.command()
 async def unload(ctx, extension):
     '''卸載特定抹組'''
-    if ctx.author.id == 315414910689476609:
-        bot.unload_extension(f'cmds.{extension}')
-        await ctx.send(f'{extension} 模組卸載完成')
-    else:
-        await ctx.send('只有牛牛能用這個指令')
+    bot.unload_extension(f'cmds.{extension}')
+    await ctx.send(f'{extension} 模組卸載完成')
+
 @commands.check(check_owner)
 @bot.command()
 async def reload(ctx, extension):
     '''重新載入特定模組'''
-    if ctx.author.id == 315414910689476609:
-        bot.reload_extension(f'cmds.{extension}')
-        await ctx.send(f'{extension} 模組重新載入完成')
-    else:
-        await ctx.send('只有牛牛能用這個指令')
+    bot.reload_extension(f'cmds.{extension}')
+    await ctx.send(f'{extension} 模組重新載入完成')
 
 @commands.check(check_owner)
 @bot.command()
 async def poweroff(ctx):
     '''關閉bot'''
-    if ctx.author.id == 315414910689476609:
-        await ctx.send('bot關閉中...')
-        exit()
-    else:
-        await ctx.send('只有牛牛能用這個指令')
+    await ctx.send('bot關閉中...')
+    exit()
 
 for filename in os.listdir('./cmds'):
     if filename.endswith('.py'):
