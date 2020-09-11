8e2abe00f849b2dbd485e07d506af1bfc84e9bf4
cow03haha <44705326+cow03haha@users.noreply.github.com>
2020-09-11 23:20:16 +0800

    Update task.py

--- cmds/task.py
+++ cmds/task.py
@@ -9,26 +9,45 @@ class Task(Cog_Extension):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
-        '''async def interval():
+        '''
+        async def interval():
             await self.bot.wait_until_ready()
             self.channel = self.bot.get_channel(741685158335479950)
             while not self.bot.is_closed():
                 await self.channel.send('loop test')
                 await asyncio.sleep(5)
             
-        self.bg_task = self.bot.loop.create_task(interval())'''
+        self.bg_task = self.bot.loop.create_task(interval())
+        '''
         
         #self.counter = 0
-        '''
+        
         async def good_morning():
             await self.bot.wait_until_ready()
-            self.channel = self.bot.get_channel(621994307431301130)
-            
+
             while not self.bot.is_closed():
                 now_time = datetime.datetime.now().strftime('%H%M%S')
 
-                if now_time == '080000':
-                    await self.channel.send('早')
+                if now_time == '000000':
+                    print('you got it')
+                    with open('members.json', 'r', encoding='utf8') as bcfile:
+                        bcdata = json.load(bcfile)
+
+                    for i in bcdata["member_id"]:
+                        print(i)
+                        if bcdata[f'{i}']["today"] != 1:
+                            print(bcdata[f'{i}']["total"])
+                            bcdata[f'{i}']["total"] = 0
+                            bcdata[f'{i}']["today"] = 0
+                            with open('members.json', 'w', encoding='utf8') as bcfile:
+                                json.dump(bcdata, bcfile, indent=4)
+                            print('if success')
+                        else:
+                            bcdata[f'{i}']["today"] = 0
+                            with open('members.json', 'w', encoding='utf8') as bcfile:
+                                json.dump(bcdata, bcfile, indent=4)
+                            print('else success')
+                        
                     await asyncio.sleep(1)
                     self.bg_task = self.bot.loop.create_task(good_morning())
                 else:
@@ -36,12 +55,14 @@ class Task(Cog_Extension):
                     pass
 
         self.bg_task = self.bot.loop.create_task(good_morning())
+        
+
         '''
-        '''async def fight_task():
+        async def fight_task():
             await self.bot.wait_until_ready()
 
             with open('settings.json', 'r', encoding='utf8') as bcfile:
-                        bcdata =json.load(bcfile)
+                bcdata =json.load(bcfile)
             self.guild = self.bot.get_guild(int(bcdata['fight_guild']))
             self.channel = self.guild.get_channel(int(bcdata['fight_channel']))
             self.role = self.guild.get_role(743668426383294495)
@@ -78,8 +99,10 @@ class Task(Cog_Extension):
                     await asyncio.sleep(1)
                     pass
 
-        self.bg_task = self.bot.loop.create_task(fight_task())
+            self.bg_task = self.bot.loop.create_task(fight_task())
+        '''
 
+    '''
     @commands.command()
     async def set_channel(self, ctx, ch: int):
         self.channel = self.bot.get_channel(ch)
@@ -95,7 +118,8 @@ class Task(Cog_Extension):
         bcdata['task_time'] = time
 
         with open('settings.json', 'w', encoding='utf8') as bcfile:
-            json.dump(bcdata, bcfile, indent=4)'''
+            json.dump(bcdata, bcfile, indent=4)
+    '''
 
 def setup(bot):
     bot.add_cog(Task(bot))
