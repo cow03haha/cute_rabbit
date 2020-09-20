1228e0a2713d87dd64fc40f22ff828632fa1970f
cow03haha <44705326+cow03haha@users.noreply.github.com>
2020-09-21 00:17:27 +0800

    Update admin.py

--- cmds/admin.py
+++ cmds/admin.py
@@ -69,7 +69,7 @@ class Admin(Cog_Extension):
             await msg.channel.send(embed=embed)
         
         #簽到系統
-        if msg.channel.id == 753923849480962100 and msg.content == '簽' and msg.author.bot == False:
+        if msg.channel.id == 753543338006806528 and msg.content == '簽' and msg.author.bot == False:
             with open('members.json', 'r', encoding='utf8') as bcfile:
                 bcdata =json.load(bcfile)
             
