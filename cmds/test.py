d39acce3139fddf10deda5ed40604e21ba33a94f
cow03haha <44705326+cow03haha@users.noreply.github.com>
2020-10-06 19:28:29 +0800

    Update test.py

--- cmds/test.py
+++ cmds/test.py
@@ -5,19 +5,13 @@ from bot import check_owner
 
 class Test(Cog_Extension):
     '''test only'''
-
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        if type(error) in [discord.ext.commands.errors.CheckFailure, discord.ext.commands.errors.MissingPermissions]:
-            await ctx.send("你的權限不足以使用此指令")
-        else:
-            await ctx.send(f'不明錯誤，如持續出現請聯絡 <@315414910689476609> 並提供以下資訊\n```{error}\n{type(error)}```')
     
     @commands.command()
     @commands.check(check_owner)
     async def test(self, ctx):
         '''for test'''
-        await ctx.send("OOF")
+        await ctx.send(f'{ctx.command.help}')
+        print(ctx.message.author.mention)
 
 def setup(bot):
     bot.add_cog(Test(bot))
