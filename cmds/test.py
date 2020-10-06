5ff60c67a00fdf8d3203a13d20a299a8d0787a27
cow03haha <44705326+cow03haha@users.noreply.github.com>
2020-10-06 12:52:52 +0800

    Create test.py

---
+++ cmds/test.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+from cores.classes import Cog_Extension
+from bot import check_owner
+
+class Test(Cog_Extension):
+    '''test only'''
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if type(error) in [discord.ext.commands.errors.CheckFailure, discord.ext.commands.errors.MissingPermissions]:
+            await ctx.send("你的權限不足以使用此指令")
+        else:
+            await ctx.send(f'不明錯誤，如持續出現請聯絡 <@315414910689476609> 並提供以下資訊\n```{error}\n{type(error)}```')
+    
+    @commands.command()
+    @commands.check(check_owner)
+    async def test(self, ctx):
+        '''for test'''
+        await ctx.send(f'{datetime.datetime.now().strftime("%Y-%m-%d")} 結算成功')
+
+def setup(bot):
+    bot.add_cog(Test(bot))
