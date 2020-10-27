1bb73623477a035c0ac67271a1c8507b1aa0e5c2
cow03haha <44705326+cow03haha@users.noreply.github.com>
2020-10-27 17:35:57 +0800

    Update test.py

--- cmds/test.py
+++ cmds/test.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from cores.classes import Cog_Extension
 from bot import check_owner
+import asyncio
 
 class Test(Cog_Extension):
     '''test only'''
@@ -10,7 +11,21 @@ class Test(Cog_Extension):
     @commands.check(check_owner)
     async def test(self, ctx):
         '''for test'''
-        if 0:
-            await ctx.send("1")
+        # Gets voice channel of message author
+        voice_channel = ctx.author.channel
+        channel = None
+        if voice_channel != None:
+            channel = voice_channel.name
+            vc = await voice_channel.connect()
+            vc.play(discord.FFmpegPCMAudio(executable="C:/ffmpeg/bin/ffmpeg.exe", source="C:<path_to_file>"))
+            # Sleep while audio is playing.
+            while vc.is_playing():
+                sleep(.1)
+            await vc.disconnect()
+        else:
+            await ctx.send(str(ctx.author.name) + "is not in a channel.")
+        # Delete command after the audio is done playing.
+        await ctx.message.delete()
+
 def setup(bot):
     bot.add_cog(Test(bot))
