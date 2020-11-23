ba6e488d7eed3eb084301aadedb69bb8555d7a09
cow03haha <44705326+cow03haha@users.noreply.github.com>
2020-11-23 16:15:38 +0800

    Update test.py

--- cmds/test.py
+++ cmds/test.py
@@ -3,26 +3,39 @@ from discord.ext import commands
 from cores.classes import Cog_Extension
 from bot import check_owner
 import asyncio
+import os
 
 class Test(Cog_Extension):
     '''test only'''
     
     @commands.command()
     @commands.check(check_owner)
-    async def test(self, ctx):
+    async def test(self, ctx, *, name):
         '''for test'''
         # Gets voice channel of message author
-        voice_channel = ctx.author.voice.channel
+        songs = []
+        for filename in os.listdir('./audio'):
+            if filename.endswith('.webm'):
+                songs.append(filename[:-5])
+        
+        vc = ctx.author.voice
 
-        if voice_channel != None:
-            vc = await voice_channel.connect()
-            vc.play(discord.FFmpegPCMAudio(executable="C:\\Python\\Python37\\Scripts\\ffmpeg.exe", source="audio/fbi open up.webm"))
+        if vc != None:
+            if name == "list":
+                await ctx.send("\n".join(songs))
+                return
+            if name not in songs:
+                await ctx.send("歌曲庫裡沒有這首歌")
+                return
+            
+            vc_channel = await vc.channel.connect()
+            vc_channel.play(discord.FFmpegPCMAudio(executable="C:\\Python\\Python37\\Scripts\\ffmpeg.exe", source=f'audio/{name}.webm'))
             # Sleep while audio is playing.
-            while vc.is_playing():
+            while vc_channel.is_playing():
                 await asyncio.sleep(0.1)
-            await vc.disconnect()
+            await vc_channel.disconnect()
         else:
-            await ctx.send(str(ctx.author.name) + "is not in a channel.")
+            await ctx.send(ctx.author.name + " is not in a channel.")
 
         # Delete command after the audio is done playing.
         #await ctx.message.delete()
